69a12062022bbcb22ab9e77aed07798e4df8d1d1
StarLicker <bendriggs99@gmail.com>
2025-03-21 19:15:59 -0600

    Added main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+"""
+
+import os
+import sys
+import pandas as pd
+
+
+def main():
+    # save cell-count input file
+    cc_file = sys.argv[1]
+
+
+if __name__ == "__main__":
+    main()
+    
